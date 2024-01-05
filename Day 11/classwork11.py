5fdf8eb05dde9cf3eafd4762853e8377a828a970
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:27:37 +0400

    11th commit

---
+++ Day 11/classwork11.py
@@ -0,0 +1,30 @@
+# old_list = [2, 4, 6, 2, 4, 7, 2, 9]
+# new_list = []
+# for num in old_list:
+#     if num != 4:
+#         new_list.append(num)
+# print(new_list)
+
+
+# print("{0}{1}{0}".format("abra", "cad"))
+#Display Paralelepipedi
+# print("{0}{1}{1}{2}".format("para".capitalize(), "le", "pipedi"))
+#Display Penguin
+# print("{0}{1}{2}{3}{4}{5}{6}".format("p".capitalize(), "e", "n", "g", "u", "i", "n"))
+
+# x = ", ".join(["nika", "mate", "data"])
+# print(x)
+
+# x = "some text goes here"
+# spl = x.split(" ")
+# print(spl)
+
+
+
+
+
+
+
+
+
+
---
+++ Day 11/homework11.py
@@ -0,0 +1,19 @@
+#Creating function with 3 parameter(Triangle pages) and create code that prints if that triangle can exist or not.
+
+samkutxedis_gverdebi = ["samkutxedis_gverdi1", "samkutxedis_gverdi2", "samkutxedis_gverdi3"]
+def samkutxedi(samkutxedis_gverdebi):
+    if len(samkutxedis_gverdebi) == 3:
+        print("aseti samkutxedi iarsebebs".capitalize())
+    elif len(samkutxedis_gverdebi) != 3:
+        print("aseti samkutxedi ver iarsebebs".capitalize())
+samkutxedi(samkutxedis_gverdebi)
+
+#Done
+
+#Creating function with 3 elements and display it like list.
+
+saxelebi = ("Nika, Data, Giorgi")
+spl = saxelebi.split(' ')
+print(spl)
+
+#Done
