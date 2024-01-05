156ae990ed0f3016dc5043e65c34ea91d3573069
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:26:33 +0400

    9th commit

---
+++ Day 9/classwork9.py
@@ -0,0 +1,70 @@
+# from turtle import *
+
+# def draw_square():
+#     for i in range(4):
+#         forward(100)
+#         left(90)
+
+# def kalmis_wageba(x,y):
+#     penup()
+#     goto(x,y)
+#     pendown()
+
+# draw_square()
+# kalmis_wageba(0,200)
+
+# draw_square()
+# kalmis_wageba(-200,200)
+
+# draw_square()
+# kalmis_wageba(-200,0)
+
+# draw_square()
+
+# exitonclick()
+
+
+# numbers = range(10)
+# print(numbers)
+
+# x = range(5)
+# print(x)
+
+# numbers = list(range(10))
+# print(numbers)
+
+# nums = list(range(5, 12))
+# print(nums[4])
+
+nums = [4, 5, 6, 7, 8]
+res = nums[::-1]
+print(res)
+
+# animals =["cat", "dog", "bird", "cow"]
+# print(animals[-1])
+
+# vehicle = 'airplane'
+# vehicle[:3] = 'water'
+# print(vehicle)
+
+
+# cities = ['Berlin', 'Munich']
+# print(cities[1:])
+
+# range()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day 9/test.py
@@ -0,0 +1,25 @@
+#სასტუმროში შემოდის 4 ბიჭი და გვინდა ოთხივეს დავუპრინტოთ მოგესალმებით, დღეში სამჯერ
+
+# def bichebtan_misalmeba():
+#     print("hello gio")
+#     print("hello nika")
+#     print("hello tornike")
+#     print("hello luka")
+
+# print("Dila")
+# bichebtan_misalmeba()
+# print("Shuadge")
+# bichebtan_misalmeba()
+# print("Sagamo")
+# bichebtan_misalmeba()
+
+def misalmeba(saxeli, age):
+    print("Mogesalmebi" + saxeli)
+    print("Sheni asakia" , age)
+
+misalmeba(" nika", 50)
+misalmeba(" luka", 40)
+misalmeba(" gabriel", 30)
+misalmeba(" sandro", 20)
+
+
