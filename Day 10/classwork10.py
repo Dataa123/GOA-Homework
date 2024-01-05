3084d516d872b48350ab5a427da8350ffe7bb22c
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:27:03 +0400

    10th commit

---
+++ Day 10/classwork10.py
@@ -0,0 +1,73 @@
+# print("SmArTpHoNe".lower())
+# print("SmArTpHoNe".upper())
+
+
+# print("Bee".find("e"))
+
+
+# surname = ("keshelava")
+# print(len(surname))
+
+# nums = [1, 2, 3 ,4, 7, 1]
+# print(len(nums))
+# set_of_nums = set(nums)
+# print(set_of_nums)
+
+# letters = ["a", "b", "c"]
+# letters += ["d"]
+# print(len(letters))
+# print(letters)
+
+# str = "some text".capitalize()
+# x = len(str)
+# print(str)
+# print(x)
+
+# x = "abc"
+# x *= 2
+# print(len(x))
+
+# nums = [1, 2, 3]
+# nums.append(4)
+# print(nums)
+
+# words = ["python", "fun"]
+# words.insert(1, "is")
+# print(words)
+
+# family = ["nino", "amirani"]
+# family.append("luka")
+# family.insert(1, "data")
+# print(family)
+
+# list = [1, 2, 3, 4]
+
+# print(min(list))
+# print(max(list))
+
+# nums = [1, 3, 5, 2, 4]
+
+# res = min(nums) + max(nums)
+
+# print(res)
+
+# nums = [4, 5, 6]
+
+# msg = "numbers: {0} {1} {2}" . format(nums[0], nums[1], nums[2])
+
+# print(msg)
+
+# family = ["deda", "mama", "dzma"]
+# fam = "my mom is: {0}, my dad is: {1}, my brother is: {2}," . format(family[0], family[1], family[2])
+# print(fam)
+
+# a = "my name is: {} my surname is: {}".format("Data", "Diasamidze")
+# print(a)
+
+family = ["nino", "amirani", "luka"]
+fam = "my mom is: {0}, my dad is: {1}, my brother is: {2}".format(family[0], family[1], family[2])
+
+print(fam)
+
+
+
