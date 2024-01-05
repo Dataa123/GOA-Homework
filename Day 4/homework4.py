6142d03c4e7ff7441ecd123cf18adeb7dc73a894
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:21:12 +0400

    4th commit

---
+++ Day 4/homework4.py
@@ -0,0 +1,22 @@
+#creating variables from ages of my family members
+father_age = 46
+mother_age = 40
+brother_age = 13
+
+#updating variables
+new_father_age = (father_age + 20)
+new_mother_age = (mother_age + 20)
+new_brother_age = (brother_age + 20)
+
+#displaying new ages
+print(new_father_age)
+print(new_mother_age)
+print(new_brother_age)
+
+#creating questions that user has to answer, than displaying information with the answers user gave.
+father_age = input("what is your father's age?")
+print("he is " + father_age + " years old")
+mother_age = input("how old is your mother?")
+print("she is " + mother_age + " years old")
+brother_age = input("how old is your brother?")
+print("he is " + brother_age + " years old")
