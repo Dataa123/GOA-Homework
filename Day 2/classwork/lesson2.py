5d7724058493af8e5423701cda73fbb0c805349e
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:17:42 +0400

    third commit

---
+++ Day 2/classwork/lesson2.py
@@ -0,0 +1,14 @@
+PC_price = 10000
+Discount = 10 # % discount
+Available_money = 9600
+
+
+print(Available_money - PC_price*(100-Discount)/100)
+
+
+
+
+#create variables from ages from your family members
+#gamoitvalet ramdenjer agemateba tqveni mamis asaki tqvens asaks 10 wlis shemdeg
+
+
---
+++ Day 2/homework/homework2.py
@@ -0,0 +1,11 @@
+#create variables from ages from your family members (1)
+#gamoitvalet ramdenjer agemateba tqveni mamis asaki tqvens asaks 10 wlis shemdeg (2)
+
+#(1)
+Father = 46
+Mother = 40
+Brother = 13
+Me = 16
+
+#(2)
+print((Father+10)/(Me+10))
