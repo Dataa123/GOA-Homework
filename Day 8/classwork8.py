6e2e5a1d3cf430aa9f20d271600b0ecdd5becded
Dataa123 <Diasamidzed53@gmail.com>
2024-01-05 22:23:46 +0400

    8th commit

---
+++ Day 8/classwork8.py
@@ -0,0 +1,35 @@
+# nums = [1, 2, 3, 4, 5]
+# nums[2] = 1920
+# print(nums)
+
+# nums = [1, 2, 3, 4, 5]
+# nums[4] = nums[0]
+# print(nums[4])
+
+# nums = [1, 2, 3]
+# print(nums + [4, 5, 6])
+
+# nums = [4, 6, 7]
+# nums += [8, 10, 12]
+# print(nums[3]//nums[0])
+
+# nums = [1, 2, 3]
+# print(nums * 3)
+
+# products = ["juice", "chocolate", "water"]
+
+# choice = int(input("choose one"))
+# print(products[choice])
+
+# for i in range(0,20,2):
+#     print(i)
+
+# names = ["nika", "gio", "dato"]
+
+# for element in names:
+#     print(element)
+
+for l in "nika":
+    print(l)
+
+
